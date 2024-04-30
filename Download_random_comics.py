9ec195e229bd558a9161ffdbd656b3b2f5ad4119
skripko-a <skripkoae@gmail.com>
2024-04-30 20:23:11 +0400

    add script for download xkcd comics with random number

---
+++ Download_random_comics.py
@@ -0,0 +1,49 @@
+import os
+from random import randint
+from urllib.parse import urlsplit
+import requests
+
+
+def get_comics_json(comics_number):
+    url = f'https://xkcd.com/{comics_number}/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    payload_json = response.json()
+    return payload_json
+
+
+def get_comics_max_number():
+    url = 'https://xkcd.com/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    payload_json = response.json()
+    comics_max_number = payload_json['num']
+    return comics_max_number
+
+
+def download_comics(comics_url, comics_filename):
+    response = requests.get(comics_url)
+    response.raise_for_status()
+    with open(comics_filename, 'wb') as file:
+        file.write(response.content)
+
+
+def pick_comics_extension(comics_url):
+    path = urlsplit(comics_url).path
+    comics_extension = os.path.splitext(path)[1]
+    return comics_extension
+
+
+def main():
+    comics_number = randint(0, get_comics_max_number())
+    comics_json = get_comics_json(comics_number)
+    author_comics_comment = comics_json['alt']
+    comics_url = comics_json['img']
+    comics_title = comics_json['title']
+    comics_extension = pick_comics_extension(comics_url)
+    comics_filename = f'{comics_number}-{comics_title}{comics_extension}'
+    download_comics(comics_url, comics_filename)
+
+
+if __name__ == '__main__':
+    main()
