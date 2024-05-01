5268febf83aa721570c7df0d91684f4ca4f2b8d4
skripko-a <skripkoae@gmail.com>
2024-05-01 11:24:53 +0400

    add script for posting comics by telegram_bot

--- Download_random_comics.py
+++ Download_random_comics.py
@@ -21,29 +21,22 @@ def get_comics_max_number():
     return comics_max_number
 
 
-def download_comics(comics_url, comics_filename):
-    response = requests.get(comics_url)
-    response.raise_for_status()
-    with open(comics_filename, 'wb') as file:
-        file.write(response.content)
-
-
 def pick_comics_extension(comics_url):
     path = urlsplit(comics_url).path
     comics_extension = os.path.splitext(path)[1]
     return comics_extension
 
 
-def main():
+def download_comics():
     comics_number = randint(0, get_comics_max_number())
     comics_json = get_comics_json(comics_number)
     author_comics_comment = comics_json['alt']
     comics_url = comics_json['img']
     comics_title = comics_json['title']
     comics_extension = pick_comics_extension(comics_url)
-    comics_filename = f'{comics_number}-{comics_title}{comics_extension}'
-    download_comics(comics_url, comics_filename)
-
-
-if __name__ == '__main__':
-    main()
+    comics_filename = f'comics_dir/{comics_number}-{comics_title}{comics_extension}'
+    response = requests.get(comics_url)
+    response.raise_for_status()
+    with open(comics_filename, 'wb') as file:
+        file.write(response.content)
+    return comics_filename, author_comics_comment
