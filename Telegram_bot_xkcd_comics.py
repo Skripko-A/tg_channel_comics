b1f37d9d12f41411be17fd43a4c3dd0c4463566b
skripko-a <skripkoae@gmail.com>
2024-05-01 14:49:12 +0400

    add typehints, rename comment arg into caption

--- Telegram_bot_xkcd_comics.py
+++ Telegram_bot_xkcd_comics.py
@@ -5,16 +5,16 @@ from dotenv import load_dotenv
 from Download_random_comics import download_comics
 
 
-def bot_post(chat_id, bot, comics_filename,  comment):
+def bot_post(chat_id: str, bot: telegram.bot.Bot, comics_filename: str,  caption: str):
     with open(comics_filename, 'rb') as photo:
-        bot.send_photo(chat_id=chat_id, photo=photo, caption=comment)
+        bot.send_photo(chat_id=chat_id, photo=photo, caption=caption)
 
 
 def main():
     load_dotenv()
-    chat_id = os.environ['XKCD_COMICS_CHAT_ID']
-    bot_token = os.environ['XKCD_COMICS_BOT_TOKEN']
-    bot = telegram.Bot(bot_token)
+    chat_id: str = os.environ['XKCD_COMICS_CHAT_ID']
+    bot_token: str = os.environ['XKCD_COMICS_BOT_TOKEN']
+    bot: telegram.bot.Bot = telegram.Bot(bot_token)
     comics_filename, comment = download_comics()
     bot_post(chat_id, bot, comics_filename,  comment)
     os.remove(comics_filename)
