9076d95954bdb2cacfae50f65b4029a864c56cb4
skripko-a <skripkoae@gmail.com>
2024-05-02 11:48:52 +0400

    update functions name

--- Download_random_comics.py
+++ Download_random_comics.py
@@ -4,7 +4,7 @@ from urllib.parse import urlsplit
 import requests
 
 
-def get_comics_data(comics_number: int) -> dict:
+def request_for_comics_info(comics_number: int) -> dict:
     url = f'https://xkcd.com/{comics_number}/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
@@ -29,12 +29,12 @@ def pick_comics_extension(comics_url: str) -> str:
 
 def download_comics() -> (str, str):
     comics_number: int = randint(0, get_comics_max_number())
-    comics_data: dict = get_comics_data(comics_number)
+    comics_data: dict = request_for_comics_info(comics_number)
     author_comics_comment: str = comics_data['alt']
     comics_url: str = comics_data['img']
     comics_title: str = comics_data['title']
     comics_extension: str = pick_comics_extension(comics_url)
-    comics_filename: str = f'{comics_number}-{comics_title}{comics_extension}'
+    comics_filename: str = f'{comics_number}-{comics_title}xkcd{comics_extension}'
     response = requests.get(comics_url)
     response.raise_for_status()
     with open(comics_filename, 'wb') as file:
