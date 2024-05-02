b4125dec01b62be250c1a27d2779f43f57ff8760
skripko-a <skripkoae@gmail.com>
2024-05-02 11:54:52 +0400

    add deleting temp files as finally block for main function

--- Telegram_bot_xkcd_comics.py
+++ Telegram_bot_xkcd_comics.py
@@ -1,6 +1,6 @@
 import os
 import telegram
-
+import glob
 from dotenv import load_dotenv
 from Download_random_comics import download_comics
 
@@ -10,14 +10,22 @@ def send_photo_by_bot(chat_id: str, bot: telegram.bot.Bot, comics_filename: str,
         bot.send_photo(chat_id=chat_id, photo=photo, caption=caption)
 
 
+def clear_temp_png_files():
+    png_files = glob.glob('**/*xkcd.png', recursive=True)
+    for png_file in png_files:
+        os.remove(png_file)
+
+
 def main():
     load_dotenv()
     chat_id: str = os.environ['XKCD_COMICS_CHAT_ID']
     bot_token: str = os.environ['XKCD_COMICS_BOT_TOKEN']
     bot: telegram.bot.Bot = telegram.Bot(bot_token)
-    comics_filename, comment = download_comics()
-    send_photo_by_bot(chat_id, bot, comics_filename,  comment)
-    os.remove(comics_filename)
+    try:
+        comics_filename, comment = download_comics()
+        send_photo_by_bot(chat_id, bot, comics_filename,  comment)
+    finally:
+        clear_temp_png_files()
 
 
 if __name__ == '__main__':
